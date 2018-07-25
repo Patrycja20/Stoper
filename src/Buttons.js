6e682d256912ed66f16737428e2a9d1b6022e387
Patrycja20 <madydapatrycja97@o2.pl>
2018-07-25 16:38:36 +0200

    Proba_dodania_akcji_do_przyciskow

--- src/Buttons.js
+++ src/Buttons.js
@@ -1,10 +1,20 @@
 import React from "react";
 
-
 class Buttons extends React.Component
 {
-  render() {
-    return <div><button>Start</button><button type={"reset"}>Reset</button></div>
+    constructor() {
+        super();
+        this.state = { text: 'Start' };
+    }
+    onButtonClick() {
+        if(this.state.text=='Start') this.setState({ text: 'Stop' });
+        else this.setState({ text: 'Start' });
+    }
+    onReset(){
+
+    }
+    render() {
+        return <div><button onClick={this.onButtonClick.bind(this)}>{this.state.text}</button><button onClick={this.onReset.bind(this)}>Reset</button></div>
   }
 }
 
