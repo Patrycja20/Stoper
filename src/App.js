ebef3def25705442f2af7dcd8a18aa42dce81762
Patrycja20 <madydapatrycja97@o2.pl>
2018-07-26 20:01:57 +0200

    Dodanie sterowania czasem przez przycisk start/stop

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React from 'react';
 import Buttons from './Buttons';
 import Time from './Time';
 import './Style.css';
@@ -6,29 +6,49 @@ import './Style.css';
 
 class App extends React.Component {
 
-  constructor () {
+  constructor() {
     super();
-    this.state={
-      seconds:0,
+    this.state = {
+      seconds: 0,
+      isWorks: false,
     };
   }
 
-  componentWillMount() {
-    setInterval(this.increment,1000);
+  onStarted = () => {
+    this.uchwyt = setInterval(this.increment, 1000);
+  }
+
+  onStopped = () => {
+    clearInterval(this.uchwyt);
+}
+
+  onChangeWorks = () => {
+    if(this.state.isWorks === false) {
+      this.setState({isWorks: true});
+      this.onStarted();
+    }
+    else {
+      this.setState({isWorks:false});
+      this.onStopped();
+    }
   }
 
   render() {
     return (
       <div>
-        <hr/><h1> STOPER </h1><hr/>
+        <hr/>
+        <h1> STOPER </h1>
+        <hr/>
         <Time czas={this.state.seconds}/>
-        <Buttons/>
+        <Buttons zmien={this.onChangeWorks}/>
       </div>
     );
   }
 
 
-  increment = () => { this.setState({seconds:++this.state.seconds});  }
+  increment = () => {
+    this.setState({seconds: ++this.state.seconds});
+  }
 }
 
 
--- src/Buttons.js
+++ src/Buttons.js
@@ -1,20 +1,26 @@
 import React from "react";
 
-class Buttons extends React.Component
-{
-    constructor() {
-        super();
-        this.state = { text: 'Start' };
-    }
-    onButtonClick() {
-        if(this.state.text=='Start') this.setState({ text: 'Stop' });
-        else this.setState({ text: 'Start' });
-    }
-    onReset(){
+class Buttons extends React.Component {
+  constructor() {
+    super();
+    this.state = {text: 'Start'};
+  }
 
-    }
-    render() {
-        return <div><button onClick={this.onButtonClick.bind(this)}>{this.state.text}</button><button onClick={this.onReset.bind(this)}>Reset</button></div>
+  onButtonClick() {
+    if (this.state.text === 'Start') this.setState({text: 'Stop'});
+    else this.setState({text: 'Start'});
+    this.props.zmien();
+  }
+
+  onReset() {
+
+  }
+
+  render() {
+    return <div>
+      <button onClick={this.onButtonClick.bind(this)}>{this.state.text}</button>
+      <button onClick={this.onReset.bind(this)}>Reset</button>
+    </div>
   }
 }
 
